bb9c0bde54bff176cc5c234765c53bb4331609d4
Florian Mounier <paradoxxx.zero@gmail.com>
2021-09-13 17:12:05 +0200

    [ADD] sale_coupon_custom_code_format module

---
+++ sale_coupon_custom_code_format/__manifest__.py
@@ -0,0 +1,8 @@
+{
+    "name": "Human Coupon",
+    "summary": "Allow for masked format random coupons.",
+    "version": "14.0.1.0.0",
+    "category": "Sales",
+    "depends": ["sale_coupon"],
+    "license": "LGPL-3",
+}
