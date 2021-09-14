0cf4b4bcd79b3d932f2fc8e3accdf4f217e9bb22
Florian Mounier <paradoxxx.zero@gmail.com>
2021-09-14 10:16:51 +0200

    [ADD] sale_coupon_deferred_coupon_dedup module

---
+++ sale_coupon_deferred_coupon_dedup/__manifest__.py
@@ -0,0 +1,11 @@
+# Copyright 2021 Akretion - Florian Mounier
+{
+    "name": "Deferred Coupon Duplication Check",
+    "summary": "Allows having duplicate coupons in quotation but not in sales order.",
+    "version": "14.0.1.0.0",
+    "category": "Sales",
+    "website": "https://github.com/OCA/sale-promotion",
+    "depends": ["sale_coupon"],
+    "author": "Akretion, Odoo Community Association (OCA)",
+    "license": "LGPL-3",
+}
