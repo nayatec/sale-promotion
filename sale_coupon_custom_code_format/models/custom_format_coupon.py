a8044ae63c2449432016b4f6d89367fd280fdc1f
nayatec <43960749+nayatec@users.noreply.github.com>
2021-09-16 15:12:30 +0200

    Update custom_format_coupon.py
    fix typo
    
    Update custom_format_coupon.py
    
    just a proposal for performance sake :-)

--- sale_coupon_custom_code_format/models/custom_format_coupon.py
+++ sale_coupon_custom_code_format/models/custom_format_coupon.py
@@ -9,36 +9,35 @@ class CustomFormatCoupon(models.Model):
     _inherit = "coupon.coupon"
 
     _code_mask = "XXXXXX-00"
-    _forbidden_characters = "iI1oO0"
+    _forbidden_characters = frozenset("iI1oO0")
     _dedup_max_retries = 20
 
-    def _generate_code_from_mask(self):
+    def _generate_code_from_mask(self, choices):
         def unmask(char):
-            # x means a random lowercase letter
-            if char == "x":
-                return choice(
-                    [
-                        char
-                        for char in ascii_lowercase
-                        if char not in self._forbidden_characters
-                    ]
-                )
-            # x means a random uppercase letter
-            if char == "X":
-                return choice(
-                    [
-                        char
-                        for char in ascii_uppercase
-                        if char not in self._forbidden_characters
-                    ]
-                )
-
-            # 0 means a random digit
-            if char == "0":
-                return choice(digits)
+            char_collection = choices.get(char)
+            if char_collection:
+                return choice(char_collection)
             return char
 
         return "".join(unmask(char) for char in self._code_mask)
+
+    def _get_choice_collections(self):
+        # x means a random lowercase letter
+        # X means a random uppercase letter
+        # 0 means a random digit
+        return {
+            'X': [
+                char
+                for char in ascii_uppercase
+                if char not in self._forbidden_characters
+            ],
+            'x': [
+                char
+                for char in ascii_lowercase
+                if char not in self._forbidden_characters
+            ],
+            '0': digits,
+        }
 
     @api.model
     def _generate_code(self):
@@ -46,10 +45,11 @@ class CustomFormatCoupon(models.Model):
         # Inherits https://github.com/odoo/odoo/blob/14.0/addons/coupon/models/coupon.py#L15
         # This method doesn't call super since it's only the code generation
 
-        code = self._generate_code_from_mask()
+        choices = self._get_choice_collections()
+        code = self._generate_code_from_mask(choices)
         retries = 0
         while len(self.env["coupon.coupon"].search([("code", "=", code)])):
-            code = self._generate_code_from_mask()
+            code = self._generate_code_from_mask(choices)
             retries += 1
             if retries > self._dedup_max_retries:
                 raise ValidationError(
